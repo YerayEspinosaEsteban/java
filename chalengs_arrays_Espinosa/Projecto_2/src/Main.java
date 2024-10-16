c11bd0127c4903646d6f69a75582dac90db321ac
YerayEspinosaEsteban <yeray0303@gmail.com>
2024-10-16 13:54:27 +0200

    16/10/2024

--- chalengs_arrays_Espinosa/Projecto_2/src/Main.java
+++ chalengs_arrays_Espinosa/Projecto_2/src/Main.java
@@ -1,18 +1,28 @@
 import java.util.Random;
-public class Main{
+public class Main {
     private static final int FILA = 10;
     private static final int COLUMNA = 10;
-    static Random aleatorio = new Random()
-    private static char [][] tablero;
-
-    public static void main(String[] args) {
-        tablero = new char[FILA][COLUMNA];
-
+    private static final int[][] matrizNumAleatorio = new int[FILA][COLUMNA];
+    public static int imprimirTablero(){
         for (int i = 0; i < FILA; i++) {
             for (int j = 0; j < COLUMNA; j++) {
-                tablero[i][j] = 10;
+                System.out.print("[" +matrizNumAleatorio[i][j]+"]" + " ");
+            }
+            System.out.println();
+        }
+        return 0;
+    }
+    public static int matrizNumerosAleatrios(int cant){
+        Random ale = new Random();
+        for (int i = 0; i < FILA; i++) {
+            for (int j = 0; j < COLUMNA; j++) {
+                matrizNumAleatorio[i][j] = ale.nextInt(cant);
             }
         }
-
+        return 0;
+    }
+    public static void main(String[] args) {
+        matrizNumerosAleatrios(10);
+        imprimirTablero();
     }
 }
